05e3fdf28803a85305c1a311f25ca820fa22b693
ashencat <forddagujar95@gmail.com>
2021-03-29 12:02:37 -0400

    tpc images

--- src/components/sections/Projects/ProjectListItems.js
+++ src/components/sections/Projects/ProjectListItems.js
@@ -1,11 +1,24 @@
-import ims from '../../../assets/ReactIMS0.jpg';
-import resto from '../../../assets/resto.png';
+import CS from '../../../assets/CS0.jpg';
+import CS1 from '../../../assets/CS1.png';
+import CS2 from '../../../assets/CS2.png';
+import CS3 from '../../../assets/CS3.png';
+import CS4 from '../../../assets/CS4.png';
+import CS5 from '../../../assets/CS5.PNG';
+import r1 from '../../../assets/r1.png';
+import r2 from '../../../assets/r2.png';
+import r3 from '../../../assets/r3.png';
+import r4 from '../../../assets/r4.png';
+import r5 from '../../../assets/r5.png';
 import ML from '../../../assets/ML.png';
 import ML1 from '../../../assets/ML2.gif';
 import ML2 from '../../../assets/ML1.png';
 import ML3 from '../../../assets/ML3.png';
 import ML4 from '../../../assets/ML4.png';
 import TPCFigma from '../../../assets/TPCFigma.png';
+import tpc1 from '../../../assets/tpc1.png';
+import tpc2 from '../../../assets/tpc2.png';
+import tpc3 from '../../../assets/tpc3.png';
+import tpc4 from '../../../assets/tpc4.png';
 import CardSizing from './CardSizing';
 
 export function ProjectListItems (props) {
@@ -16,7 +29,22 @@ export function ProjectListItems (props) {
         title: "Inventory Management",
         id: "01",
         img: [{
-            src: ims,
+            src: CS,
+            alt: "IMS project"
+        },{
+            src: CS1,
+            alt: "IMS project"
+        },{
+            src: CS2,
+            alt: "IMS project"
+        },{
+            src: CS3,
+            alt: "IMS project"
+        },{
+            src: CS4,
+            alt: "IMS project"
+        },{
+            src: CS5,
             alt: "IMS project"
         }],
         description:    `Capstone - This college project is a collaborative work of three students as a team. 
@@ -31,7 +59,19 @@ export function ProjectListItems (props) {
         title: "Restaurant Menu App",
         id: "02",
         img: [{
-            src: resto,
+            src: r1,
+            alt: "Restaurant Menu"
+        },{
+            src: r2,
+            alt: "Restaurant Menu"
+        },{
+            src: r3,
+            alt: "Restaurant Menu"
+        },{
+            src: r4,
+            alt: "Restaurant Menu"
+        },{
+            src: r5,
             alt: "Restaurant Menu"
         }],
         description: `Freelance - Created a simple frontend application that serves as a restaurant menu. 
@@ -75,9 +115,21 @@ export function ProjectListItems (props) {
         img: [{
             src: TPCFigma,
             alt: "Figma schematic of the project"
+        },{
+            src: tpc1,
+            alt: "TPC project"
+        },{
+            src: tpc2,
+            alt: "TPC project"
+        },{
+            src: tpc3,
+            alt: "TPC project"
+        },{
+            src: tpc4,
+            alt: "TPC project"
         }],
-        description: `FREELANCE - A collaborative work to create an app to manage a flowerpot shop. I have worked starting from designing up to the finished product.
-        I worked on several views making sure that it matches the Figma specifications. I also worked on the DB queries ensuring that the response is lightweight and only pulling necessary data.`,
+        description: `FREELANCE - A collaborative work to create an app to manage a flowerpot shop.
+        I worked on several views making sure that it matches the Figma specifications and including the DB queries ensuring that the response is lightweight and only pulling necessary data. In addition to that, I also am the one who set-up the AWS EC2 server to host the project`,
         links: [],
         cardHeight: cardHeight[3]
     },
