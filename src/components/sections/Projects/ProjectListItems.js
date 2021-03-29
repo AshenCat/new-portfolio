4d4d6a4969fb23b0916c99c9fbdad2635288fb32
AshenCat <forddagujar95@gmail.com>
2021-03-29 00:12:36 -0400

    new ml images

--- src/components/sections/Projects/ProjectListItems.js
+++ src/components/sections/Projects/ProjectListItems.js
@@ -1,6 +1,10 @@
 import ims from '../../../assets/ReactIMS0.jpg';
 import resto from '../../../assets/resto.png';
 import ML from '../../../assets/ML.png';
+import ML1 from '../../../assets/ML2.gif';
+import ML2 from '../../../assets/ML1.png';
+import ML3 from '../../../assets/ML3.png';
+import ML4 from '../../../assets/ML4.png';
 import TPCFigma from '../../../assets/TPCFigma.png';
 import CardSizing from './CardSizing';
 
@@ -33,9 +37,6 @@ export function ProjectListItems (props) {
         description: `Freelance - Created a simple frontend application that serves as a restaurant menu. 
         I have worked on Modals to open the food details, toast for notifications, chips for filtering the menu item, and the slider banner for promotions.`,
         links: [{
-            type: "Github",
-            href: "https://github.com/AshenCat/rm_client"
-        },{
             type: "Demo",
             href: "https://www.futuredining.ca"
         }],
@@ -46,12 +47,27 @@ export function ProjectListItems (props) {
         img: [{
             src: ML,
             alt: "Machine Learning Python"
+        },{
+            src: ML1,
+            alt: "Machine Learning Python in GIF"
+        },{
+            src: ML2,
+            alt: "Machine Learning Python"
+        },{
+            src: ML3,
+            alt: "Machine Learning Python"
+        },{
+            src: ML4,
+            alt: "Machine Learning Python"
         }],
         description: `COLLEGE - Whilst python isn't my most comfortable language, I've had a good time doing this project. 
         Using the Common Objects in Context (COCO) dataset, 
         I was able to detect objects in my screen in real time. Instead of inputting single image feeds, I modified it to take video feed instead and label all the objects it detects.
 `,
-        links: [],
+        links: [{
+            type: "Github",
+            href: "https://github.com/AshenCat/PythonObjectDetection"
+        }],
         cardHeight: cardHeight[2]
     },{
         title: "Tender Plant and Care",
