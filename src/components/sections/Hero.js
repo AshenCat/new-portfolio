6cf36a8db26b5c42698188b89c600574cdd4aa70
ashencat <forddagujar@gmail.com>
2021-03-31 19:56:03 -0400

    added aos animations

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -2,9 +2,16 @@ import React from 'react'
 import { overrideTheme } from '../useStyles';
 import { Grid, Typography } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core/styles';
+import Aos from 'aos';
+import "aos/dist/aos.css";
 
 function Hero(props) {
     const { classes } = props;
+
+    React.useEffect(()=>{
+        Aos.init({duration: 2000});
+    }, [])
+
     return (
         <>
             <nav className={classes.nav}>
@@ -13,12 +20,12 @@ function Hero(props) {
             <Grid container direction="column" justify="center" alignItems="center" className={classes.container}>
                 <Grid item className={classes.headNameContainer}>
                     <ThemeProvider theme={overrideTheme}>
-                        <Typography align="center" variant="h1" className={classes.name}>Klifford Agujar</Typography>
+                        <Typography align="center" variant="h1" className={classes.name}><span data-aos="fade-up">Klifford Agujar</span></Typography>
                     </ThemeProvider>
                 </Grid>
                 <Grid item>
                     <Typography align="center" variant="h4" className={classes.title}>
-                        Full Stack Developer
+                        <span data-aos="fade-up" data-aos-delay="400">Full Stack Developer</span>
                     </Typography>
                 </Grid>
             </Grid>
