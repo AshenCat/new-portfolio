0db3b8be4ee00c152143dc8f787b99d539397de2
ashencat <forddagujar@gmail.com>
2021-04-03 16:45:18 -0400

    images still exist incase of fallback

--- src/components/sections/Projects/ProjectListItems.js
+++ src/components/sections/Projects/ProjectListItems.js
@@ -1,27 +1,49 @@
-import CS0 from "../../../assets/CS0.jpg";
-import CS1 from "../../../assets/CS1.png";
-import CS2 from "../../../assets/CS2.png";
-import CS3 from "../../../assets/CS3.png";
-import CS4 from "../../../assets/CS4.png";
-import CS5 from "../../../assets/CS5.png";
-import ML from "../../../assets/ML.png";
-import ML1 from "../../../assets/ML1.png";
-import ML2 from "../../../assets/ML2.webm";
-import ML3 from "../../../assets/ML3.png";
-import ML4 from "../../../assets/ML4.png";
-import r1 from "../../../assets/r1.png";
-import r2 from "../../../assets/r2.png";
-import r3 from "../../../assets/r3.png";
-import r4 from "../../../assets/r4.png";
-import r5 from "../../../assets/r5.png";
-import tpc1 from "../../../assets/tpc1.png";
-import tpc2 from "../../../assets/tpc2.png";
-import tpc3 from "../../../assets/tpc3.png";
-import tpc4 from "../../../assets/tpc4.png";
-import TPCFigma from "../../../assets/TPCFigma.png";
+// import CS0 from "../../../assets/CS0.jpg";
+// import CS1 from "../../../assets/CS1.png";
+// import CS2 from "../../../assets/CS2.png";
+// import CS3 from "../../../assets/CS3.png";
+// import CS4 from "../../../assets/CS4.png";
+// import CS5 from "../../../assets/CS5.png";
+// import ML from "../../../assets/ML.png";
+// import ML1 from "../../../assets/ML1.png";
+// // import ML2 from "../../../assets/ML2.webm";
+// import ML3 from "../../../assets/ML3.png";
+// import ML4 from "../../../assets/ML4.png";
+// import r1 from "../../../assets/r1.png";
+// import r2 from "../../../assets/r2.png";
+// import r3 from "../../../assets/r3.png";
+// import r4 from "../../../assets/r4.png";
+// import r5 from "../../../assets/r5.png";
+// import tpc1 from "../../../assets/tpc1.png";
+// import tpc2 from "../../../assets/tpc2.png";
+// import tpc3 from "../../../assets/tpc3.png";
+// import tpc4 from "../../../assets/tpc4.png";
+// import TPCFigma from "../../../assets/TPCFigma.png";
 
 export function ProjectListItems () {
     
+    const CS0 = "https://i.imgur.com/lcRZm8O.jpg";
+    const CS1 = "https://i.imgur.com/MT2Vv4L.png";
+    const CS2 = "https://i.imgur.com/hk3Id5K.png";
+    const CS3 = "https://i.imgur.com/SJQSgpo.png";
+    const CS4 = "https://i.imgur.com/Xj7itJa.png";
+    const CS5 = "https://i.imgur.com/Jjj5FA8.png";
+    const ML = "https://i.imgur.com/v1ZlRDA.png";
+    const ML1 = "https://i.imgur.com/4zhWQeb.png";
+    const ML2 = `https://i.imgur.com/F3J0saH.mp4`
+    const ML3 = "https://i.imgur.com/c1D2LCX.png";
+    const ML4 = "https://i.imgur.com/QGSG1HT.png";
+    const r1 = "https://i.imgur.com/96nq7Aj.png";
+    const r2 = "https://i.imgur.com/y9IOei7.png";
+    const r3 = "https://i.imgur.com/K9grRpi.png";
+    const r4 = "https://i.imgur.com/QuLJQy8.png";
+    const r5 = "https://i.imgur.com/B1VENAB.png";
+    const tpc1 = "https://i.imgur.com/bpoQY6L.png";
+    const tpc2 = "https://i.imgur.com/j1lrk6j.png";
+    const tpc3 = "https://i.imgur.com/wH8ApDm.png";
+    const tpc4 = "https://i.imgur.com/e78iqIX.png";
+    const TPCFigma = "https://i.imgur.com/45Yu9Bp.png";
+
     return [{
         title: "Inventory Management",
         id: "01",
@@ -93,17 +115,17 @@ export function ProjectListItems () {
         title: "Python Object Detection",
         id: "03",
         img: [{
-            src: ML,
+            src: ML2,
             alt: "Machine Learning Python",
-            type: "image"
+            type: "video"
         },{
             src: ML1,
             alt: "Machine Learning Python in GIF",
             type: "image"
         },{
-            src: ML2,
+            src: ML,
             alt: "Machine Learning Python",
-            type: "video"
+            type: "image"
         },{
             src: ML3,
             alt: "Machine Learning Python",
